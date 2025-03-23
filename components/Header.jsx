eaa7f9142c21ead37abe94113e73c49c9512a3be
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-03-23 18:25:39 +0530

    Design Database, Create Database Models, Store User in DB

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,8 +27,10 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { dark, shadesOfPurple } from "@clerk/themes";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className=" px-4 h-16 flex items-center justify-between">
